60a69b211a0d6ebdaece0f2a8a9c04ef88ed349e
Matt Biggin <matthew.biggin@gmail.com>
2013-11-23 10:41:59 +0000

    Added a BlockingQueue implementation

--- src/com/ultraflynn/javonical/multithreading/blockqueue/BlockingQueue.java
+++ src/com/ultraflynn/javonical/multithreading/blockqueue/BlockingQueue.java
@@ -1,8 +1,31 @@
 package com.ultraflynn.javonical.multithreading.blockqueue;
 
-public final class BlockingQueue
+import java.util.LinkedList;
+import java.util.Queue;
+
+public final class BlockingQueue<T>
 {
-    public static void main(String[] args)
-    {
+  private final Queue<T> queue = new LinkedList<T>();
+
+  public synchronized T pop()
+  {
+    // It's possible for this thread to spuriously wake up so we guard against this using a
+    // "while". Had an "if" been used next time round the poll() would have returned no element
+    // and this method is intended to return a valid element or block. The "while" will keep
+    // testing and releasing the monitor until the queue has an element in it to return.
+    while (queue.size() == 0) {
+      try {
+        wait();
+      } catch (InterruptedException e) {
+        return null;
+      }
     }
+    return queue.remove();
+  }
+
+  public synchronized void push(T element)
+  {
+    queue.add(element);
+    notifyAll();
+  }
 }
---
+++ src/com/ultraflynn/javonical/multithreading/blockqueue/Consumer.java
@@ -0,0 +1,11 @@
+package com.ultraflynn.javonical.multithreading.blockqueue;
+
+import java.util.concurrent.Callable;
+
+public final class Consumer implements Callable
+{
+  public Object call() throws Exception
+  {
+    return null;  //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
